b957df9c53e9a4a82e9ad74ef4df732c46d94e24
prabhat00155 <prabhat00155@gmail.com>
2016-10-05 17:10:43 -0700

    V2

---
+++ ResumeDemo/callPredictor.py
@@ -0,0 +1,33 @@
+﻿import urllib.request
+import json
+
+def resume_predict(profile_data):
+    data = {
+        "Inputs": {
+                "input1":
+                [
+                   profile_data
+                ],
+        },
+    "GlobalParameters":  {}
+    }
+
+    body = str.encode(json.dumps(data))
+
+    url = 'https://ussouthcentral.services.azureml.net/subscriptions/a148ec5553064f8f907e0fc6415333ae/services/20a66283a7884d5481bf78d91c72aca1/execute?api-version=2.0&format=swagger'
+    api_key = 'nyOppUQx5VKT9P1t1+TGLp+UJpS9uEm2yHCP5bGlmsLPBxttvL1sDYwlRZM6jaFgSCOYFmstpmeuInAZuzFZjQ=='
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+    result = ""
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read().decode('utf-8')
+        #print(result)
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(json.loads(error.read().decode("utf8", "ignore")))
+    return result
--- ResumeDemo/routes.py
+++ ResumeDemo/routes.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, url_for, render_template
+﻿from flask import Flask, request, url_for, render_template
 from app import app
+from getLinkedInData import getData
+from callPredictor import resume_predict
+import json
 
 @app.route('/', methods = ['GET', 'POST'])
 def hello():
@@ -7,7 +10,22 @@ def hello():
     if request.method == 'GET':
         return render_template('RequestPage.html')
     elif request.method == 'POST':
-
-        return render_template('ResponsePage.html')
+        handle = request.form['name']
+        try:
+            profile_data = getData(handle)
+        except:
+             return render_template('Invalid.html')
+        profile_data["unit_id"] = "1"
+        profile_data["decision"] = ""
+        prediction = resume_predict(profile_data)
+        if prediction == "":
+            return render_template('Invalid.html')
+        prediction = json.loads(prediction)
+        label = prediction['Results']['output1'][0]['PredictedLabel']
+        predictedLabel = "Accept" if label == "False" else "Reject"   
+        score = float(prediction['Results']['output1'][0]['Probability'])
+        if score < 0.5:
+            score = 1 - score
+        return render_template('ResponsePage.html', label = predictedLabel, score = int(score * 100))
     else:
         return "<h2>Error in request!</h2>"
